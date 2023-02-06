0d4b2101fe72e9a8d0b4e97d2a23384dc2c20e6f
kerdamon <kwalas314@gmail.com>
2023-02-06 09:04:00 +0000

    Add second solution ant complexity analysis to task 1

--- tasks/0001.py
+++ tasks/0001.py
@@ -1,4 +1,8 @@
-def solve(input, k):
+'''
+Took me: 36 min 30 sec for solve_method_1 to work properly, but went through solve_method_2 during process of solving task.
+'''
+
+def solve_method_1(input, k):
     input = sorted(input)
 
     l = 0
@@ -11,7 +15,6 @@ def solve(input, k):
         return False
 
     while(l!=p):
-        print(f'l={l} p={p}')
         if input[l] + input[p] == k:
             return True
         if input[l] + input[p] > k:
@@ -21,9 +24,19 @@ def solve(input, k):
 
     return False
 
+def solve_method_2(input, k):
+    seen = set(input[:1])
+    for elem in input:
+        if (k - elem) in seen:
+            return True
+        seen.add(elem)
+    return False
+
+def solve(input, k):
+    return solve_method_1(input, k)
+
 if __name__=='__main__':
     input = [10,15,3,7]
     k = 17
-    # corner cases: two numbers the same
     print(solve(input, k))
     
